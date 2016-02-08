dbe8559c703ba8052ea9fac525e962f9e21eb623
openku <worasait.s@ku.th>
2016-02-08 09:56:50 +0700

    Update certify.js

--- intaff/certify.js
+++ intaff/certify.js
@@ -1,5 +1,22 @@
-function test() {
-  saveUrlToDrive('https://www.dropbox.com/s/v66wvckwpcznsp6/lock.png?dl=1', 'temp', 'lock')
+// before running this script
+//   # create a folder 'certify' in Google Drive
+
+function onSubmit(e) {
+  var resp = e.response.getItemResponses();
+  var url = convertToDownload(resp[5].getResponse());
+  var name = resp[0].getResponse();
+  var superEmail = resp[4].getResponse();
+  var superName = resp[3].getResponse();
+  
+  // save image to drive
+  saveUrlToDrive(url, 'certify', name);   
+  
+  // mail superviser
+  supUrl = 'https://docs.google.com/forms/d/1szyyOUzpxPcRrok1D4026vaV5K5iHI7TFF9p2iZ8-o8/viewform?entry.1142428611=supname&entry.1139297885=worker'
+           .replace('supname', superName)
+           .replace('worker', name)
+  MailApp.sendEmail(superEmail, 'ผู้บังยืนยัน', 'เรียน ' + superName + '\n\n'
+                   + 'ตามที่ ' + encodeURI(supUrl));
 }
 
 function saveUrlToDrive(url, folder, filename) {
@@ -12,7 +29,6 @@ function saveUrlToDrive(url, folder, filename) {
   var folder = DriveApp.getFoldersByName(folder).next();
   folder.addFile(file);
   DriveApp.getRootFolder().removeFile(file);
-  //file.setTrashed(true);
 }
 
 function convertToDownload(url) {
@@ -24,7 +40,12 @@ function convertToDownload(url) {
   //   https://cloudbox.ku.ac.th/public.php?service=files&t=7f618ac8474e6a7119ddab1c1e82aa44
   //   to
   //   https://cloudbox.ku.ac.th/public.php?service=files&t=7f618ac8474e6a7119ddab1c1e82aa44&download
-  
+  if (url.slice(-4) == 'dl=0') {
+    url = url.slice(0, -4) + 'dl=1';
+  } else if (url.indexOf('//cloudbox.ku.ac.th') != -1) {
+    if (url.slice(-1 * '&download'.length) != '&download') {
+  	  url += '&download';
+    }
+  }
+  return url;
 }
-
-
